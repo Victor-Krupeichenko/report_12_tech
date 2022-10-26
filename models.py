c8bc34f084f7a16fa767b1efbc318e0e72a4132e
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2022-10-26 22:05:41 +0300

    Модель базы данных (Database model)

---
+++ models.py
@@ -0,0 +1,26 @@
+from peewee import *
+
+database = SqliteDatabase('database.db')  # с какой имено будет взаимадействовать наше приложение
+
+
+class BaseModel(Model):
+    id = AutoField(unique=True)  # автозаполнение и уникальное
+
+    class Meta:
+        database = database  # с какой бд будет взаимодействовать
+        order_by = 'id'  # по какому полю проводить сортировку
+
+
+class Report(BaseModel):
+    year = CharField()  # год
+    month = CharField()  # месяц
+    total_spend = IntegerField()  # израсходовано всего (т.у.т. -целые)
+    fraction_spend = FloatField()  # израсходовано всего (т.у.т. -дробная часть)
+    released_population = IntegerField()  # отпущено населению(т.у.т. - целые)
+    fraction_rel_pop = FloatField()  # отпущено населению(т.у.т. - дробная часть)
+    thousand_kilowatt_hours = IntegerField()  # тыс.квт/ч.
+    integer_standard_fuel_ton = IntegerField()  # тыс.квт/ч. в т.у.т (целые)
+    fraction_standard_fuel_ton = FloatField()  # тыс.квт/ч. в т.у.т (дробные)
+
+    class Meta:
+        table_name = 'reports_12_tek'
