63bee9823535e431d2ff9e4c3cb56d64cef39527
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2022-10-26 22:09:31 +0300

    Предварительно переписано в класс Добавлено: тестовое запись в базу данных бесконечный цикл для предварительной записи базы данных (Pre-rewritten to class Added: test write to database infinite loop for prewriting database)

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import datetime
 from docxtpl import DocxTemplate
+from models import *
+
+# Создание БД если таковая отсуствует
+with database:
+    database.create_tables([Report])  # создание таблицы
+print('DONE')
+# --- # --- # --- # --- #
 
 # получение даты
 months_list = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь',
@@ -10,83 +17,90 @@ year = now.year
 day = now.day
 number_month = now.month
 month = months_list[now.month - 1]  # заполняется месяц в котором сдается отчет
+# TODO менять оканчание месяца
 months = months_list[now.month - 2]  # заполняется месяц за который сдается отчет
+
 # --- # --- # --- # --- #
 
-# answer = input("Хочешь дополнить БД (ДА - д)/(НЕТ - н):   ").lower()
-# if answer[0] == "д" or answer[0] == 'l':
-#     print("OK")
-#     # TODO заполнение данных в БД
-# else:
-#     print("NO")
-
-COEFFICIENT = 0.2345
-COEFFICIENT_ENERGY = 0.123
-
-# Запись данных в word файл
 answer_total = 0
 answer_released = 0
 answer_energy = 0
 
-while True:
-    try:
-        answer_total = float(input(f"Израсходовано всего м3 за {months} {year}г.:   "))
-        break
-    except ValueError:
-        print("Ошибка ввода")
-        continue
 
-while True:
-    try:
-        answer_released = float(input(f"Отпущено населению м3 за {months} {year}г.:   "))
-        break
-    except ValueError:
-        print("Ошибка ввода")
-        continue
-
-while True:
-    try:
-        answer_energy = float(input(f"тыс.квт/ч. за {months} {year}г.:   "))
-        break
-    except ValueError:
-        print("Ошибка ввода")
-        continue
+start_populating_database = True  # Запуск предворительного заполнения БД (в бесконечном цикле while)
 
 
-def total_spend_released(meters, coefficient):
-    """Получает из м3 т.у.т."""
-    tmp = str(meters * coefficient).split(".")
-    return int(tmp[0]), round(float(f"0.{tmp[1]}"), 3)
+class Reports:
+    def __init__(self):
+        self.coefficient = 0.2345
+        self.coefficient_energy = 0.123
+        self.year = now.year
+        self.day = now.day
+        self.months = months_list[now.month - 2]
 
+    def populating_database(self):
+        """
+        Заполнение БД
+        """
+        answer = input("Хочешь дополнить БД (ДА - д)/(НЕТ - н):   ").lower()
+        if answer[0] == "д" or answer[0] == "l":
+            # TODO заполнение данных в БД
+            Report.create(
+                total_spend=32, fraction_spend=0.52, released_population=15, fraction_rel_pop=0.15,
+                thousand_kilowatt_hours=777,
+                integer_standard_fuel_ton=3, fraction_standard_fuel_ton=0.03, year=year, month=f"январь - {months}"
+            )
+            # TODO пробовать задавать вопросы в бесконечном цикле для заполнения БД
+        else:
+            print("NO")
+            global start_populating_database
+            start_populating_database = False
+            return start_populating_database
 
-def total_spend_energy(kwt, coefficient):
-    """Получение из квт/ч т.у.т."""
-    tmp = str(kwt * coefficient / 1000).split(".")
-    return int(tmp[0]), round(float(f"0.{tmp[1]}"), 3)
+    def total_spend_released(self, meters):
+        """Переводит м3 в т.у.т.
+        :param meters: m3
+        :return: Возвращает два значения: целою и дробную часть в т.у.т.
+        (Converts m3 to t.c.f.
+        Returns two values integer and fractional part in t.c.f.)
+        """
+        tmp = str(meters * self.coefficient).split(".")
+        return int(tmp[0]), float(f"0.{tmp[1]}")
 
+    def total_spend_energy(self, kwt):
+        """Переводит тыс.квт/ч. в т.у.т.
+        :param kwt:
+        :return: Возвращает два значения: целою и дробную часть в т.у.т.
+        """
+        tmp = str(kwt * self.coefficient_energy / 1000).split(".")
+        return int(tmp[0]), float(f"0.{tmp[1]}")
 
-total_spent, fraction_spent = total_spend_released(answer_total,
-                                                   coefficient=COEFFICIENT)
-released_population, fraction_released = total_spend_released(answer_released,
-                                                              coefficient=COEFFICIENT)
-total_energy, fraction_energy = total_spend_energy(answer_energy,
-                                                   coefficient=COEFFICIENT_ENERGY)
+    def docx_save(self, m, ms):
+        """
+        Запись данных в word.docx
+        :return: новый заполненый документ word
+        """
+        doc = DocxTemplate("starting.docx")
+        # TODO добавить остольные параметры в context
+        context = {
+            "month": m,
+            "months": ms,
+            "year": self.year,
+            "day": self.day,
+        }
+        doc.render(context)
+        doc.save(f"№{number_month - 1} январь - {months} {self.year}.docx")
 
-print(total_spent, fraction_spent, sep='\n')
-print(released_population, fraction_released, sep='\n')
-print(total_energy, fraction_energy, sep='\n')
 
 # TODO записыва тыс.квт/ч. в колонку нужно как-то правильно
 
-# Запись данных в word.doc
-doc = DocxTemplate("starting.docx")
-context = {
-    'month': month,
-    'months': months,
-    'year': year,
-    'day': day,
-}
-doc.render(context)
-doc.save(f"№{number_month - 1} январь - {months} {year}.docx")
+temp = Reports()
 
-# ---  # --- # --- # --- #
+# Запуск предворительного заполнения БД в бесконечном цикле while
+while start_populating_database:
+    if start_populating_database:
+        temp.populating_database()
+    else:
+        print(start_populating_database)
+        break
+# --- # --- # --- # --- #
