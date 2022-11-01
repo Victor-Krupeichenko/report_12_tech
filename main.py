cdf49af5bd6af121f25d8042a5ec092b2afb4ad1
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2022-11-02 00:46:20 +0300

    Почти полностью переписан код (Almost completely rewritten code)

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from models import *
 # Создание БД если таковая отсуствует
 with database:
     database.create_tables([Report])  # создание таблицы
-print('DONE')
+print('loaded...'.upper())
 # --- # --- # --- # --- #
 
 # получение даты
@@ -19,12 +19,9 @@ number_month = now.month
 month = months_list[now.month - 1]  # заполняется месяц в котором сдается отчет
 # TODO менять оканчание месяца
 months = months_list[now.month - 2]  # заполняется месяц за который сдается отчет
-
+print(months)
 # --- # --- # --- # --- #
 
-answer_total = 0
-answer_released = 0
-answer_energy = 0
 
 start_populating_database = True  # Запуск предворительного заполнения БД (в бесконечном цикле while)
 
@@ -37,60 +34,51 @@ class Reports:
         self.day = now.day
         self.months = months_list[now.month - 2]
 
-    def populating_database(self):
-        """
-        Заполнение БД
-        """
-        answer = input("\nХочешь дополнить БД (ДА - д)/(НЕТ - н):   ").lower()
-        if answer[0] == "д" or answer[0] == "l":
-            answer_year = input("Год:   ")
-            answer_month = input("Месяц:   ")
-            answer_spend = input("Израсходовано всего в т.у.т.:   ")
-            answer_population = input("Отпущено населению в т.у.т:   ")
-            answer_kilowatt = input("тыс.квт/ч.:   ")
-            answer_consumption = input("Суммарное потребление тэр:   ")
-            answer_db_save = input("\nСохранить эти данные в БД (ДА - д)/(НЕТ - н):   ").lower()
-            if answer_db_save[0] == "д" or answer_db_save[0] == "l":
+    def questions_report(self):
+        """Вопросы для предварительного заполнения БД"""
+        answer_year = input("Год:   ")
+        answer_month = input("Месяц:   ")
+        answer_spend = input("Израсходовано всего в т.у.т.:   ")
+        answer_population = input("Отпущено населению в т.у.т:   ")
+        answer_kilowatt = input("тыс.квт/ч.:   ")
+        answer_consumption = input("Суммарное потребление тэр:   ")
+        return [answer_year, answer_month, answer_spend, answer_population, answer_kilowatt, answer_consumption]
+
+    def db_previously(self):
+        """Предварительное заполнение БД"""
+        answer_previously = input("\n" + r"Предварительно заполнить БД - ДА(д) \ НЕТ(н):   ").lower()
+        if answer_previously[0] == "д" or answer_previously[0] == "l":
+            tmp = self.questions_report()
+            save_to_db = input("\n" + r"Сохранить эти данные в БД - ДА(д) \ НЕТ(н):   ").lower()
+            if save_to_db[0] == "д" or save_to_db[0] == 'l':
                 Report(
-                    total_spend=answer_spend, fraction_spend=0, released_population=answer_population,
-                    fraction_rel_pop=0,
-                    thousand_kilowatt_hours=answer_kilowatt,
-                    integer_standard_fuel_ton=0, fraction_standard_fuel_ton=0, year=answer_year,
-                    month=f"январь - {answer_month}", total_consumption=answer_consumption
+                    year=tmp[0], month=f"январь - {tmp[1]}", total_spend=tmp[2], released_population=tmp[3],
+                    thousand_kilowatt_hours=tmp[4], total_consumption=tmp[5], fraction_spend=0, fraction_rel_pop=0,
+                    integer_standard_fuel_ton=0, fraction_standard_fuel_ton=0
                 ).save()
-            else:
-                print("Отмена")
         else:
             global start_populating_database
             start_populating_database = False
             return start_populating_database
 
-    def fill_report(self):
-        """Заполнение отчета"""
-        answer_fill_report = input("\nПриступаем к заполнению отчёта (ДА - д)/(НЕТ - н):   ").lower()
-        answer_m3 = 0
-        answer_m3_released = 0
-        if answer_fill_report[0] == "д" or answer_fill_report[0] == "l":
-            while True:
-                try:
-                    answer_m3 = float(input(f"Израсходовано всего м3 за {self.months} {self.year}г.:   "))
-                    break
-                except ValueError:
-                    print("Ошибка ввода")
-                    continue
-            while True:
-                try:
-                    answer_m3_released = float(input(f"Отпущено населению м3 за {self.months} {self.year}г.:   "))
-                    break
-                except ValueError:
-                    print("Ошибка ввода")
-                    continue
-            t_s, t_f = self.total_spend_released(answer_m3)
-            r_p, r_f = self.total_spend_released(answer_m3_released)
-            # TODO нужно как-то правильно добавить данные в БД
-            # TODO прежде чем сохранить данные в БД необходимо слажить дробные части т.у.т. полученые из БД
+    def getting_latest_record(self):
+        """Получение последней записи из БД"""
+        tmp = Report.select().order_by(Report.id.desc()).get_or_none()
+        tmp_list = [tmp.year, tmp.month, tmp.total_spend, tmp.released_population, tmp.thousand_kilowatt_hours,
+                    tmp.total_consumption, tmp.fraction_spend, tmp.fraction_rel_pop, tmp.integer_standard_fuel_ton,
+                    tmp.fraction_standard_fuel_ton]
+        return tmp_list
+
+    def getting_corresponding_period(self):
+        """Получение данных за соответствующий период"""
+        tmp = Report.select().where((Report.year == f"{self.year + 1}") & (Report.month == f"январь - {self.months}"))
+        if tmp:
+            t_list = []
+            for i in tmp:
+                t_list.extend([i.total_spend, i.released_population, i.thousand_kilowatt_hours, i.total_consumption])
+            print(t_list[0], t_list[1], t_list[2], t_list[3])
         else:
-            print("Тогда выходим")
+            print(f"Данные за период январь - {self.months} {self.year - 1}г. в БД отсутствуют!")
 
     def total_spend_released(self, meters):
         """Переводит м3 в т.у.т.
@@ -107,7 +95,7 @@ class Reports:
         :param kwt:
         :return: Возвращает два значения: целою и дробную часть в т.у.т.
         """
-        tmp = str(kwt * self.coefficient_energy / 1000).split(".")
+        tmp = str(kwt * 1000 * self.coefficient_energy / 1000).split(".")
         return int(tmp[0]), float(f"0.{tmp[1]}")
 
     def docx_save(self, m, ms):
@@ -118,24 +106,22 @@ class Reports:
         doc = DocxTemplate("starting.docx")
         # TODO получить данные за соответствующий период
         # TODO добавить остольные параметры в context
+        tmp = self.getting_latest_record()
         context = {
             "month": m,
             "months": ms,
             "year": self.year,
             "day": self.day,
+            "t": tmp[4]
         }
         doc.render(context)
-        doc.save(f"№{number_month - 1} январь - {months} {self.year}.docx")
-
-
-# TODO записыва тыс.квт/ч. в колонку нужно как-то правильно
+        doc.save(f"№ {number_month - 1} январь - {months} {self.year}.docx")
 
 
 if __name__ == "__main__":
     temp = Reports()
     # Запуск предворительного заполнения БД в бесконечном цикле while
-    while start_populating_database:
-        temp.populating_database()
-    # Запуск заполнения отчета
-    temp.fill_report()
+    # while start_populating_database:
+    #     temp.db_previously()
+    temp.getting_corresponding_period()
 # --- # --- # --- # --- #
