c651c3d0b4c51eddab681a5b5606735c999eb8b2
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2022-11-02 21:27:51 +0300

    метод Сложение прошлых и текущих данных (method Addition of past and current data)

--- main.py
+++ main.py
@@ -64,19 +64,21 @@ class Reports:
     def getting_latest_record(self):
         """Получение последней записи из БД"""
         tmp = Report.select().order_by(Report.id.desc()).get_or_none()
-        tmp_list = [tmp.year, tmp.month, tmp.total_spend, tmp.released_population, tmp.thousand_kilowatt_hours,
-                    tmp.total_consumption, tmp.fraction_spend, tmp.fraction_rel_pop, tmp.integer_standard_fuel_ton,
-                    tmp.fraction_standard_fuel_ton]
+        tmp_list = [tmp.year, tmp.month, tmp.total_spend, tmp.fraction_spend, tmp.released_population,
+                    tmp.fraction_rel_pop, tmp.thousand_kilowatt_hours,
+                    tmp.integer_standard_fuel_ton,
+                    tmp.fraction_standard_fuel_ton, tmp.total_consumption]
         return tmp_list
 
     def getting_corresponding_period(self):
         """Получение данных за соответствующий период"""
-        tmp = Report.select().where((Report.year == f"{self.year + 1}") & (Report.month == f"январь - {self.months}"))
+        tmp = Report.select().where((Report.year == f"{self.year - 1}") & (Report.month == f"январь - {self.months}"))
         if tmp:
             t_list = []
             for i in tmp:
                 t_list.extend([i.total_spend, i.released_population, i.thousand_kilowatt_hours, i.total_consumption])
             print(t_list[0], t_list[1], t_list[2], t_list[3])
+            return t_list
         else:
             print(f"Данные за период январь - {self.months} {self.year - 1}г. в БД отсутствуют!")
 
@@ -98,21 +100,37 @@ class Reports:
         tmp = str(kwt * 1000 * self.coefficient_energy / 1000).split(".")
         return int(tmp[0]), float(f"0.{tmp[1]}")
 
-    def docx_save(self, m, ms):
+    def addition_past_current_data(self, ans_total, rel_total, energy_total):
+        """Сложение прошлых и текущих данных"""
+
+        # TODO добавить проверку если заполняется за январь
+
+        latest_record = self.getting_latest_record()
+        total_spend = self.total_spend_released(ans_total)
+        total = total_spend[0] + latest_record[2]  # израсходовано всего (прошлый  + текущи месяц)
+        released_spend = self.total_spend_released(rel_total)
+        released = released_spend[0] + latest_record[4]  # отпущено населению (прошлый + текущий месяц)
+        spend_energy = self.total_spend_energy(energy_total)
+        energy = energy_total + latest_record[6]  # тыс.квт/ч. (прошлый + текущий месяц)
+        print(total, released, energy, spend_energy)
+
+    def docx_save(self, m, ms, get_s, get_p, get_k, get_c):
         """
         Запись данных в word.docx
         :return: новый заполненый документ word
         """
         doc = DocxTemplate("starting.docx")
-        # TODO получить данные за соответствующий период
         # TODO добавить остольные параметры в context
-        tmp = self.getting_latest_record()
+
         context = {
             "month": m,
             "months": ms,
             "year": self.year,
             "day": self.day,
-            "t": tmp[4]
+            "get_s": get_s,
+            "get_p": get_p,
+            "get_k": get_k,
+            "get_c": get_c,
         }
         doc.render(context)
         doc.save(f"№ {number_month - 1} январь - {months} {self.year}.docx")
@@ -123,5 +141,35 @@ if __name__ == "__main__":
     # Запуск предворительного заполнения БД в бесконечном цикле while
     # while start_populating_database:
     #     temp.db_previously()
-    temp.getting_corresponding_period()
+    # g_c_p = temp.getting_corresponding_period()  # получение данных за соответствующий период прошлого года
+    # temp.docx_save(m=month, ms=months, get_s=g_c_p[0], get_p=g_c_p[1], get_k=g_c_p[2], get_c=g_c_p[3])
+    answer_total = 0  # Израсходовано всего
+    answer_released = 0  # Отпущено населению
+    answer_energy = 0  # тыс.квт/ч.
+
+    while True:
+        try:
+            answer_total = float(input(f"Израсходовано всего м3 за {months} {year}г.:   "))
+            break
+        except ValueError:
+            print("Ошибка ввода")
+            continue
+
+    while True:
+        try:
+            answer_released = float(input(f"Отпущено населению м3 за {months} {year}г.:   "))
+            break
+        except ValueError:
+            print("Ошибка ввода")
+            continue
+
+    while True:
+        try:
+            answer_energy = int(input(f"тыс.квт/ч. за {months} {year}г.:   "))
+            break
+        except ValueError:
+            print("Ошибка ввода")
+            continue
+
+    temp.addition_past_current_data(ans_total=answer_total, rel_total=answer_released, energy_total=answer_energy)
 # --- # --- # --- # --- #
