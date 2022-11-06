91a1aa3516746acd0707fe7baac8f3e4a83242be
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2022-11-06 14:08:49 +0300

    изменена база данных (database changed)

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from peewee import *
 
-database = SqliteDatabase('database.db')  # с какой имено будет взаимадействовать наше приложение
+database = SqliteDatabase('12_tek_database.db')  # с какой имено будет взаимадействовать наше приложение
 
 
 class BaseModel(Model):
@@ -13,13 +13,9 @@ class Report(BaseModel):
     year = CharField()  # год
     month = CharField()  # месяц
     total_spend = IntegerField()  # израсходовано всего (т.у.т. -целые)
-    fraction_spend = FloatField()  # израсходовано всего (т.у.т. -дробная часть)
     released_population = IntegerField()  # отпущено населению(т.у.т. - целые)
-    fraction_rel_pop = FloatField()  # отпущено населению(т.у.т. - дробная часть)
     thousand_kilowatt_hours = IntegerField()  # тыс.квт/ч.
-    integer_standard_fuel_ton = IntegerField()  # тыс.квт/ч. в т.у.т (целые)
-    fraction_standard_fuel_ton = FloatField()  # тыс.квт/ч. в т.у.т (дробные)
-    total_consumption = IntegerField()  # суммарное потребление тэр
+    total_consumption = IntegerField()  # израсходовано всего (total_spend + integer_standard_fuel_ton)
 
     class Meta:
         table_name = 'reports_12_tek'
